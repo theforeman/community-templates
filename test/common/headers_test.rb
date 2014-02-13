8a0792f7f9f73351f94efc23d564c609bd4cdb86
Lukas Zapletal <lzap+git@redhat.com>
2014-02-13 09:31:32 +0100

    Adding tests for header parsing

---
+++ test/common/headers_test.rb
@@ -0,0 +1,20 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'test_helper'
+require 'yaml'
+
+class TestHeaders < MiniTest::Unit::TestCase
+
+  def test_headers
+    Dir.glob("**/*.erb") do |erb|
+      extracted = IO.read(erb).match(/<%\#(.+?).-?%>/m)
+      refute_nil extracted
+      yaml = YAML.load(extracted[1])
+      refute_nil yaml
+      refute_nil yaml["kind"]
+      refute_empty yaml["kind"]
+      refute_nil yaml["name"]
+      refute_empty yaml["name"]
+    end
+  end
+end
