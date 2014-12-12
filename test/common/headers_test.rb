357b0389ef4f5ab059c635ce19d198f00633a61d
Michael Moll <kvedulv@kvedulv.de>
2014-12-12 15:10:32 +0100

    normalize quoting

--- test/common/headers_test.rb
+++ test/common/headers_test.rb
@@ -6,15 +6,15 @@ require 'yaml'
 class TestHeaders < MiniTest::Unit::TestCase
 
   def test_headers
-    Dir.glob("**/*.erb") do |erb|
-      extracted = IO.read(erb).match(/<%\#(.+?).-?%>/m)
+    Dir.glob('**/*.erb') do |erb|
+      extracted = IO.read(erb).match(/<%#(.+?).-?%>/m)
       refute_nil extracted
       yaml = YAML.load(extracted[1])
       refute_nil yaml
-      refute_nil yaml["kind"]
-      refute_empty yaml["kind"]
-      refute_nil yaml["name"]
-      refute_empty yaml["name"]
+      refute_nil yaml['kind']
+      refute_empty yaml['kind']
+      refute_nil yaml['name']
+      refute_empty yaml['name']
     end
   end
 end
