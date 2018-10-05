aa58234ec5bede2692c2a325f9f85097dd9dce11
Marek Hulán <ares@users.noreply.github.com>
2018-10-05 17:18:37 +0200

    Add report templates (#523)

--- test/common/headers_test.rb
+++ test/common/headers_test.rb
@@ -6,13 +6,20 @@ require 'yaml'
 class TestHeaders < Minitest::Test
 
   def test_headers
-    Dir.glob('**/*.erb') do |erb|
+    Dir.glob('provisioning_templates/**/*.erb') do |erb|
       extracted = IO.read(erb).match(/<%#(.+?).-?%>/m)
       refute_nil extracted
       yaml = YAML.load(extracted[1])
       refute_nil yaml
       refute_nil yaml['kind'], "#{erb} metadata does not contain kind attribute"
       refute_empty yaml['kind'], "#{erb} metadata kind attribute is empty"
+    end
+
+    Dir.glob('**/*.erb') do |erb|
+      extracted = IO.read(erb).match(/<%#(.+?).-?%>/m)
+      refute_nil extracted
+      yaml = YAML.load(extracted[1])
+      refute_nil yaml
       refute_nil yaml['name'], "#{erb} metadata does not contain name attribute"
       refute_empty yaml['name'], "#{erb} metadata name attribute is empty"
       refute_nil yaml['model'], "#{erb} metadata does not contain model attribute"
